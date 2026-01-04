bd4254726cb5bb2b2caec34b57384c263b79e62d
mitra42 <mitra@mitra.biz>
2026-01-04 20:07:39 +1100

    Update cache so mobile SHOULD recache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // Change this version number if want existing installs of PWA to refresh their cache
 // TODO-28 - probably make this the same as the app version in frugal-iot-client
-const CACHE_NAME = 'frugal-iot-cache-v1';
+const CACHE_NAME = 'frugal-iot-cache-0.1.19';
 const urlsToCache = [
     '/',
     '/index.html',
